f158c44cc648cc480b59d59a7c2d4ceeb7a131ea
d3akhtar <danyal.akhtar@torontomu.ca>
2024-04-03 19:01:27 -0400

    Small changes to getCommentsForBug.js to work with the old viewBug.html page

--- public/Scripts/js/getCommentsForBug.js
+++ public/Scripts/js/getCommentsForBug.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Function to fetch comments from the server
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to fetch comments from the server
     const id = localStorage.getItem("bug_id");
     document.getElementById("bug-number-display").innerText = id;
     function fetchComments() {
@@ -32,9 +32,9 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-const reportDetails = ["author", "title", "body", "dateAdded"];
+    const reportDetails = ["author", "title", "body", "dateAdded"];
 
-  // Call fetchComments when the page is loaded
+    // Call fetchComments when the page is loaded
     fetchComments()
         .then(async items => {
             // Get the container div
@@ -42,30 +42,42 @@ const reportDetails = ["author", "title", "body", "dateAdded"];
 
             // Loop through the items array and create a div for each item
 
-	for (let i = 0; i < items.length; i++) {
-		if (i==0){	
-			let j = 0;
-			for (const key in items[i]) {
-				document.getElementById(reportDetails[j]).innerText = items[i][key];
-				j++;
-			}
-		} else{
-		const item = items[i];
-		const tr = document.createElement('tr');
-		for (const key in item) {
-			const td = document.createElement('td');
-			td.innerText = item[key];
-			tr.appendChild(td);
-		}
-		container.appendChild(tr);
-}
-	}
+            for (let i = 0; i < items.length; i++) {
+                if (i == 0) {
+                    container.innerHTML +=
+                `
+                    <div class="container bug-description-block" style="margin-top:80px; display:flex-box; width:70%; background-color: lightsalmon; padding: 80px; border: 4px solid black; border-radius: 30px;">
+                            <div class="bug-description">
+                                <h2 class="text-dark" style="font-weight:700">${items[i].comment_title}</h2>
+                            </div>
+                            <br>
+                            <div class="bug-description">
+                                <h3 class="text-dark">Details:</h3>
+                                <p style="color: darkred;">${items[i].comment_body}</p>
+                            </div><br><br>
+                            <div class="buttons" style="display:flex; width:70%; margin:auto">
+                                <button class="btn-outline-success" id="resolve-btn" style="width:250px; height:50px; margin: 0 auto">Resolve</button><br><br>
+                                <button class="btn-outline-primary" id="add-activity-btn" style="width:250px; height:50px; margin: 0 auto">Add Activity</button>
+                            </div>
+                        </div>
+                `
+                } else {
+                    container.innerHTML +=
+                    `
+                        <div class="container contribution-block p-4 w-70">
+                            <h3 class="text-dark" style="font-weight:800; font-size: 40px;">${items[i].comment_title} - ${items[i].comment_dateAdded}</h3>
+                            <h4 class="text-dark">${items[i].author_username}</h4>
+                            <p>${items[i].comment_body}</p>
+                        </div>
+                    `
+                }
+            }
 
-	const stat = await checkBugStatus(id);
-	if (!stat) {
-		// hide add and resolve buttons
-		document.getElementById('updateBugButton').style.display = 'block';
-		document.getElementById('resolveBugButton').style.display = 'block';
-	}
-});
+            const stat = await checkBugStatus(id);
+            if (!stat) {
+                // hide add and resolve buttons
+                document.getElementById('updateBugButton').style.display = 'block';
+                document.getElementById('resolveBugButton').style.display = 'block';
+            }
+        });
 });
