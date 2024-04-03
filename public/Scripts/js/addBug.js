064c74e5df410d36b40fdd648786632363f9019c
d3akhtar <danyal.akhtar@torontomu.ca>
2024-04-03 14:45:43 -0400

    Add bug works now, however add bug button doesn't redirect back to main.html

---
+++ public/Scripts/js/addBug.js
@@ -0,0 +1,27 @@
+document.getElementById('addReport').addEventListener('click', () => {
+  const bugTitle = document.getElementById('bugTitle').value;
+  const bugDescription = document.getElementById('bugDescription').value;
+  console.log(bugTitle);
+  console.log(bugDescription);
+
+  // Make an AJAX request to the server-side endpoint
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('POST', '/addBugAndComment', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+        alert("Report added successfully");
+        // Optionally, do something with the response
+      } else {
+        alert("error adding bug");
+        console.error('Error:', xhr.status);
+        // Optionally, handle errors
+      }
+    }
+  };
+  xhr.send(JSON.stringify({ title: bugTitle, description: bugDescription }));
+  window.location.href("main.html");
+});
