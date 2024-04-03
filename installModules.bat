0def5dc01a210c04708809bfb7e0635ba90026ab
d3akhtar <danyal.akhtar@torontomu.ca>
2024-04-03 00:24:40 -0400

    Added Ahmad's server.js files and other related files and made small changes

---
+++ installModules.bat
@@ -0,0 +1,1 @@
+npm install express && npm install express-session && npm install mysql && npm install mysql2 && npm fund
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js', // Entry point of your application
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
